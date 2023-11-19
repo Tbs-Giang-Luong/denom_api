31e84d52bdd06c1c1a040f28d73dd60442712023
Tbs-Giang-Luong <luongthanhgiangtbs@gmail.com>
2023-11-19 10:09:40 +0700

    first comment

---
+++ index.js
@@ -0,0 +1,105 @@
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import dotenv from "dotenv";
+import axios from "axios";
+import * as cheerio from "cheerio";
+import { load } from "cheerio";
+
+const app = express();
+app.use(cors());
+app.use(morgan("combined"));
+const port = 3000;
+const urlApi = "https://kimetsu-no-yaiba.fandom.com/wiki/Kimetsu_no_Yaiba_Wiki";
+const figureApi = "https://kimetsu-no-yaiba.fandom.com/wiki/";
+
+app.get("/v1/:keyword", (req, resp) => {
+  const titles = [];
+  const valueTitles = [];
+  const characterObject = {};
+  const characters = [];
+  const galleries = [];
+  const figure = figureApi + req.params.keyword;
+  try {
+    axios(figure).then(res => {
+      const result = res.data;
+      const $ = cheerio.load(result);
+      //getGallery
+
+      $(".wikia-gallery-item", result).each(function() {
+        const gallery = $(this).find("a > img").attr("data-src");
+        galleries.push(gallery);
+      });
+
+      $("aside", result).each(function() {
+        //getImage
+        const image = $(this).find("img").attr("src");
+        //get keyCharector
+        $(this).find("section > div > h3").each(function() {
+          titles.push($(this).text());
+        });
+
+        //getValue cherector
+        $(this).find("section > div > div").each(function() {
+          valueTitles.push($(this).text());
+        });
+
+        for (let i = 0; i < titles.length; i++) {
+          characterObject[titles[i].toLowerCase()] = valueTitles[i];
+        }
+        if (image) {
+          console.log(galleries);
+          characters.push({
+            name: req.params.keyword.replace("_", ""),
+            characterObject,
+            image: image,
+            gallery: galleries
+          });
+        }
+      });
+      resp.status(200);
+      resp.json(characters);
+      // console.log(titles);
+      // console.log(valueTitles);
+    });
+  } catch (error) {
+    resp.status(500);
+    resp.json(error);
+  }
+});
+
+app.get("/v1", (req, resp) => {
+  const limit = Number(req.query.limit);
+
+  const thumbnails = [];
+  try {
+    axios.get(urlApi).then(res => {
+      const result = res.data;
+      const $ = cheerio.load(result);
+      $(".portal").each(function() {
+        const name = $(this).find("a").attr("title");
+        const url = $(this).find("a").attr("href");
+        const image = $(this).find("a > img").attr("src");
+        thumbnails.push({
+          name: name,
+          url: "http://localhost:8000/v1" + url.split("wiki")[1],
+          image: image
+        });
+      });
+      resp.status(200);
+
+      if (limit) {
+        const resultLimit = thumbnails.slice(0, limit);
+        resp.json(resultLimit);
+      } else {
+        resp.json(thumbnails);
+      }
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Example app listening on port ${port}`);
+});
